d068382653d80371a75fe6d6b6a03d3f6542cebd
PranjalKalbag <pranjal.kalbag@gmail.com>
2020-04-30 09:01:19 +0530

    Fixed Recursion Issue

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -11,47 +11,46 @@ grid = [[5,3,0,0,7,0,0,0,0],
 
 
 
-def solve(grid): 
-    emp = empty(grid)
+def solve(): 
+    emp = empty()
     if not emp:
-         return True
+        return True
     else:
         row, col = emp
     for x in range (1,10):
-        if possible(row, col, x, grid):
+        if possible(row, col, x):
             grid[row][col] = x
-        if solve(grid):
-            return True
-        
-        grid[row][col] = 0
+            if solve():
+                return True	        
+    grid[row][col] = 0
     return False
 
-def possible(x,y,n, grid):
-     for i in range(0,9):
-          if grid[x][i] == n and y!=i: 
-               return False
-          if grid[i][y] == n and x!=i:
-               return False
-     x0=(x//3)*3
-     y0=(y//3)*3
-     for i in range(y0*3,y0*3+3):
-          for j in range(x0*3,x0*3+3):
-               if grid[i][j] == n:
-                    return False
-     return True
+def possible(x,y,n):
+    for i in range(0,9):
+        if grid[x][i] == n and y!=i: 
+            return False
+        if grid[i][y] == n and x!=i:
+            return False
+    x0=((x+1)//3)*3
+    y0=((y+1)//3)*3
+    for i in range(y0-3,y0):
+        for j in range(x0-3,x0):
+            if grid[i][j] == n and y!=i and x!=j:
+                return False
+    return True
  
     
-def printer(grid):
+def printer():
     print(np.matrix(grid))
 
-def empty(grid):
+def empty():
     for x in range(9):
         for y in range(9):
             if grid[x][y] == 0:
                 return(x,y)
+  
 
-
-printer(grid)
-solve(grid)
+printer()
+solve()
 print("Solved Sudoku is:")
-printer(grid)
+printer()
